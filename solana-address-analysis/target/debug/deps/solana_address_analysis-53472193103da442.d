184bf642d55e6c5547ac1fc5cbfe31b9166d9971
fauxfire <andyweng97@gmail.com>
2022-12-08 18:46:55 -0500

    fixed directory structure

--- solana-address-analysis/target/debug/deps/solana_address_analysis-53472193103da442.d
+++
@@ -1,5 +0,0 @@
-/home/fauxfire/6mv/solana-user-fraud/solana-address-analysis/target/debug/deps/solana_address_analysis-53472193103da442.rmeta: src/main.rs
-
-/home/fauxfire/6mv/solana-user-fraud/solana-address-analysis/target/debug/deps/solana_address_analysis-53472193103da442.d: src/main.rs
-
-src/main.rs:
