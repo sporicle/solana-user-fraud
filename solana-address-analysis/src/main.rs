24cbf34e12ba155441ef7ecdc6bf307fd0548867
sporicle <andyweng97@gmail.com>
2023-07-05 23:12:06 -0700

    added user usage timestamping

--- solana-address-analysis/src/main.rs
+++ solana-address-analysis/src/main.rs
@@ -8,52 +8,54 @@ use solana_transaction_status::UiMessage;
 use solana_transaction_status::UiTransactionEncoding;
 use std::collections::HashMap;
 use std::str::FromStr;
+use chrono::{DateTime, Utc, Timelike};
 
 fn main() {
     let url = "https://api.mainnet-beta.solana.com";
-
-    // Contract ID for Lifinity Protocol
-    let contract_id = Pubkey::from_str("AvtfUvU3byPXgp6Dpw3mgKB2BbVwQvGyry9KeMzD9BLc").unwrap();
-
     let commitment_config = CommitmentConfig::processed();
     let client = RpcClient::new_with_commitment(url, commitment_config);
 
-    println!(
-        "[LOG] Getting recent signatures for contract: {}",
-        contract_id
-    );
-    let res = client.get_signatures_for_address(&contract_id).unwrap();
-    println!("[LOG] Transactions fetched: {:?}", res.len());
-    let bar = ProgressBar::new(res.len().try_into().unwrap());
+    analyze_user("FWMkWWVvSz7cVkhovkVHL59TDPtX78X23bA4frTbjkCA", &client);
 
-    // key: signer | value: [signing timestamps] 
-    let mut signer_count = HashMap::new();
-    // for i in 0..res.len() {
-    for i in 0..2 {
-        bar.inc(1);
-        let (signers, ts) = get_txn_signers(&client, &res[i].signature);
-        for signer in signers {
-            let (count, timestamps) = signer_count.entry(signer).or_insert((0, Vec::new()));
-            *count += 1;
-            timestamps.push(ts);
-        }
-    }
-    bar.finish();
-    let mut count_vec: Vec<_> = signer_count.iter().collect();
+    // // Contract ID for Lifinity Protocol
+    // let contract_id = Pubkey::from_str("AvtfUvU3byPXgp6Dpw3mgKB2BbVwQvGyry9KeMzD9BLc").unwrap();
 
-    // [(signer: str, )]
-    count_vec.sort_by(|a, b| b.1 .0.cmp(&a.1 .0));
+    // println!(
+    //     "[LOG] Getting recent signatures for contract: {}",
+    //     contract_id
+    // );
+    // let res = client.get_signatures_for_address(&contract_id).unwrap();
+    // println!("[LOG] Transactions fetched: {:?}", res.len());
+    // let bar = ProgressBar::new(res.len().try_into().unwrap());
 
-    println!("{:?}", count_vec);
-    println!(
-        "[LOG] Number of unique signers in the last {} transactions: {}",
-        res.len(),
-        count_vec.len()
-    );
-    println!("{:?}", &count_vec[0]);
-    // println!("{:?}", analyze_timestamps(&count_vec[0].1.1));
-    // println!("{:?}", analyze_timestamps(&count_vec[1].1.1));
-    // println!("{:?}", analyze_timestamps(&count_vec[2].1.1));
+    // // key: signer | value: [signing timestamps] 
+    // let mut signer_count = HashMap::new();
+    // // for i in 0..res.len() {
+    // for i in 0..2 {
+    //     bar.inc(1);
+    //     let (signers, ts) = get_txn_signers(&client, &res[i].signature);
+    //     for signer in signers {
+    //         let (count, timestamps) = signer_count.entry(signer).or_insert((0, Vec::new()));
+    //         *count += 1;
+    //         timestamps.push(ts);
+    //     }
+    // }
+    // bar.finish();
+    // let mut count_vec: Vec<_> = signer_count.iter().collect();
+
+    // // [(signer: str, )]
+    // count_vec.sort_by(|a, b| b.1 .0.cmp(&a.1 .0));
+
+    // println!("{:?}", count_vec);
+    // println!(
+    //     "[LOG] Number of unique signers in the last {} transactions: {}",
+    //     res.len(),
+    //     count_vec.len()
+    // );
+    // println!("{:?}", &count_vec[0]);
+    // // println!("{:?}", analyze_timestamps(&count_vec[0].1.1));
+    // // println!("{:?}", analyze_timestamps(&count_vec[1].1.1));
+    // // println!("{:?}", analyze_timestamps(&count_vec[2].1.1));
 }
 
 fn get_txn_signers(client: &RpcClient, txn_id: &String) -> (Vec<String>, i32) {
@@ -96,4 +98,27 @@ fn analyze_timestamps(times: &Vec<i32>) -> Vec<i32> {
     }
 
     result
+}
+
+fn analyze_user(pubkey: &str, client: &RpcClient) -> i32 {
+    let user_pubkey = Pubkey::from_str(pubkey).unwrap();
+    let res = client.get_signatures_for_address(&user_pubkey).unwrap();    
+    let mut usage_counts: HashMap<u32, usize> = HashMap::new();
+
+  // time analysis
+    for i in 1..res.len() {
+        let unix_time = res[i].block_time.unwrap();
+        let utc_datetime: DateTime<Utc> = DateTime::from_utc(
+            chrono::NaiveDateTime::from_timestamp_opt(unix_time, 0).unwrap(),
+            Utc,
+        );
+        let hour = utc_datetime.hour();
+        let count = usage_counts.entry(hour).or_insert(0);
+        *count += 1;
+    }
+    let mut count_vec: Vec<_> = usage_counts.iter().collect();
+    count_vec.sort_by_key(|&(number, _count)| number);
+
+    println!("{:?}",count_vec);
+    1
 }
